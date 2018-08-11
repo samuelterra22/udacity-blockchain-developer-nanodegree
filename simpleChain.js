d5aa708865d43c3fb51653155ec5ea6175ce82ac
Samuel Terra <samuelterra22@hotmail.com>
2018-08-11 11:59:29 -0300

    Add blockchain models and logic

---
+++ simpleChain.js
@@ -0,0 +1,37 @@
+const SHA256 = require('crypto-js/sha256')
+
+/**
+ * Block Class
+ * 
+ * Class with a constructor for block
+ */
+class Block{
+    constructor(data){
+        this.hash = '',
+        this.height = 0,
+        this.body = data,
+        this.time = 0,
+        this.previousBlockhash = ''
+    }
+}
+
+/**
+ * Blockchain Class
+ * 
+ * Class with a constructor for new blockchain
+ */
+class Blockchain{
+    constructor(){
+        this.chain = []
+        this.addBlock(new Block('First block in the chain - Genesis Block'))
+    }
+    addBlock(newBlock){
+        newBlock.height = this.chain.length
+        newBlock.time = new Date().getTime().toString().slice(0,-3)
+        if(this.chain.length > 0){
+            newBlock.previousBlockhash = this.chain[this.chain.length - 1].hash
+        }
+        newBlock.hash = SHA256(JSON.stringify(newBlock)).toString()
+        this.chain.push(newBlock)
+    }
+}
