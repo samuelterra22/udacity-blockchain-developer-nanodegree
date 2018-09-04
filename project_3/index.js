a900098f9d9dee360f6e4896086ff4981ba8eeaf
Samuel Terra <samuelterra22@hotmail.com>
2018-09-04 00:12:28 -0300

    Add simple route methods

--- project_3/index.js
+++ project_3/index.js
@@ -3,6 +3,25 @@
 const express = require('express')
 const app = express()
 
-app.get('/', (req, res) => res.send('Hello World!'))
+const Blockchain = require('./Blockchain')
+const blockchain = new Blockchain()
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get('/block/:blockHeight', function (req, res) {
+  blockchain.getBlock(req.params.blockHeight).then(res => {
+    res.send(res)
+  }).catch(err => {
+    res.send(err)
+  })
+})
+
+app.post('/block', (req, res) => {
+  console.log(req.body.body)
+  res.send(JSON.stringify({}))
+
+})
