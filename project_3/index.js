586324db63f90eba39e9bf33ce7da2c1ce67fd3e
Samuel Terra <samuelterra22@hotmail.com>
2018-09-04 00:41:14 -0300

    Fix errors when get response

--- project_3/index.js
+++ project_3/index.js
@@ -5,8 +5,14 @@ const app = express()
 
 const Blockchain = require('./Blockchain')
 const blockchain = new Blockchain()
+const util = require('./util')
+const bodyParser = require('body-parser')
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
+app.use(bodyParser.json())       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+  extended: true
+}))
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
@@ -21,7 +27,11 @@ app.get('/block/:blockHeight', function (req, res) {
 })
 
 app.post('/block', (req, res) => {
-  console.log(req.body.body)
-  res.send(JSON.stringify({}))
+  if (!util.empty(req.body.body)) {
+    console.log(req.body.body)
+    res.send(JSON.stringify(req.body))
+  } else {
+    console.log('vazio')
+  }
 
 })
