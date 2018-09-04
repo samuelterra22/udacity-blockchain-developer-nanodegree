80fafef5e7389efffc3eecafa9467b7cf22b24ab
Samuel Terra <samuelterra22@hotmail.com>
2018-09-04 01:24:12 -0300

    Add comments and readme

--- project_3/index.js
+++ project_3/index.js
@@ -1,26 +1,33 @@
-// https://nordicapis.com/13-node-js-frameworks-to-build-web-apis/
+// CRITERION: Configure private blockchain project to include a RESTful API with
 
-const express = require('express')
-const app = express()
-
+// import dependencies
 const Blockchain = require('./Blockchain')
 const blockchain = new Blockchain()
 const Block = require('./Block')
 const util = require('./util')
 const bodyParser = require('body-parser')
+const path = require('path')
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'))
+const express = require('express')
+const app = express()
+
+// CRITERION: Node.js framework running on port 8000.
+app.listen(8000, () => console.log('Example app listening on port 8000!'))
 app.use(bodyParser.json())       // to support JSON-encoded bodies
 app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
   extended: true
 }))
 
+// Return the doc
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.sendFile(path.join(__dirname + '/README.md'))
 })
 
+// CRITERION: GET Block endpoint using URL path with block height parameter.
+// Preferred URL path http://localhost:8000/block/{BLOCK_HEIGHT}
 app.get('/block/:blockHeight', (req, res) => {
   blockchain.getBlock(req.params.blockHeight).then(success => {
+    // The block contents must respond to GET request with block contents in JSON format
     res.send(JSON.stringify(success))
   }).catch(error => {
     res.send(res.send(JSON.stringify(error)))
@@ -29,6 +36,7 @@ app.get('/block/:blockHeight', (req, res) => {
 
 // CRITERION: POST Block endpoint using key/value pair within request body.
 app.post('/block', (req, res) => {
+  // verify if the request ins't empty
   if (!util.empty(req.body.body)) {
 
     blockchain.addBlock(new Block(req.body.body)).then(success => {
@@ -36,10 +44,12 @@ app.post('/block', (req, res) => {
       // Note: addBlock method was modified to return the block created
       res.send(success)
     }).catch(() => {
+      // return a message in json format with error
       res.send(JSON.stringify({error: 'There was an error generating a new block'}))
     })
 
   } else {
+    // return parameter error message
     res.send(JSON.stringify({error: 'Parameter error'}))
   }
 
