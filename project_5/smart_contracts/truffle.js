040afbd0e6c3fd3f9283ee49834d2b85ccaabdb2
Samuel Terra <samuelterra22@hotmail.com>
2018-10-23 19:00:44 -0300

    Add truffle config

--- project_5/smart_contracts/truffle.js
+++ project_5/smart_contracts/truffle.js
@@ -12,7 +12,27 @@
  *   },
  */
 
+// See <http://truffleframework.com/docs/advanced/configuration>
+// to customize your Truffle configuration!
+
+var HDWalletProvider = require('truffle-hdwallet-provider')
+
+var mnemonic = 'sustain turkey until tape jelly decide grid situate inside problem orient allow'
+
 module.exports = {
-  // See <http://truffleframework.com/docs/advanced/configuration>
-  // to customize your Truffle configuration!
+  networks: {
+    development: {
+      host: '127.0.0.1',
+      port: 8545,
+      network_id: '*'
+    },
+    rinkeby: {
+      provider: function() {
+        return new HDWalletProvider(mnemonic, 'https://rinkeby.infura.io/v3/943812c88c77438392df11b19ed0c26b')
+      },
+      network_id: 4,
+      gas: 4500000,
+      gasPrice: 10000000000
+    }
+  }
 }
