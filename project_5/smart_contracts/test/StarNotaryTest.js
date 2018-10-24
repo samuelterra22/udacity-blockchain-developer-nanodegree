17fdce6d43ebaf02e7f3b95d682e29aad741b645
Samuel Terra <samuelterra22@hotmail.com>
2018-10-24 10:37:57 -0300

    Remove beforeEach

--- project_5/smart_contracts/test/StarNotaryTest.js
+++ project_5/smart_contracts/test/StarNotaryTest.js
@@ -23,49 +23,50 @@ contract('StarNotary', accounts => {
     let starId = 1
     let starPrice = web3.toWei(.01, 'ether')
 
-    beforeEach(async function() {
-      await this.contract.createStar('Star power 103!', 'I love my wonderful star', 'ra_032.155', 'dec_121.874', 'mag_245.978', { from: user1 })
-    })
-
     it('user1 can put up their star for sale', async function() {
+      await this.contract.createStar('Star power 103!', 'I love my wonderful star', 'ra_032.155', 'dec_121.874', 'mag_245.978', { from: user1 })
+
       assert.equal(await this.contract.ownerOf(starId), user1)
       await this.contract.putStarUpForSale(starId, starPrice, { from: user1 })
 
       assert.equal(await this.contract.starsForSale(starId), starPrice)
     })
 
-    describe('user2 can buy a star that was put up for sale', () => {
-      beforeEach(async function() {
-        await this.contract.putStarUpForSale(starId, starPrice, { from: user1 })
-      })
+    it('star exists', async function() {
+      await this.contract.createStar('Star power 103!', 'I love my wonderful star', 'ra_032.155', 'dec_121.874', 'mag_245.978', { from: user1 })
 
-      it('user2 is the owner of the star after they buy it', async function() {
-        await this.contract.buyStar(starId, { from: user2, value: starPrice, gasPrice: 0 })
-        assert.equal(await this.contract.ownerOf(starId), user2)
-      })
-
-      it('user2 ether balance changed correctly', async function() {
-        let overpaidAmount = web3.toWei(.05, 'ether')
-        const balanceBeforeTransaction = web3.eth.getBalance(user2)
-        await this.contract.buyStar(starId, { from: user2, value: overpaidAmount, gasPrice: 0 })
-        const balanceAfterTransaction = web3.eth.getBalance(user2)
-
-        assert.equal(balanceBeforeTransaction.sub(balanceAfterTransaction), starPrice)
-      })
+      assert.equal(await this.contract.checkIfStarExist('ra_032.155', 'dec_121.874', 'mag_245.978'), true)
     })
 
-    // checkIfStarExist test
-    it('star exists', async function() {
-        await this.contract.createStar('Star power 103!', 'I love my wonderful star', 'ra_032.153', 'dec_121.871', 'mag_245.971', { from: user1 })
-
-        assert.equal(await this.contract.checkIfStarExist('ra_032.153', 'dec_121.871', 'mag_245.971'), true)
-      })
-
     it('star has correct owner after created', async function() {
-      await this.contract.createStar('Star power 103!', 'I love my wonderful star', 'ra_032.153', 'dec_121.871', 'mag_245.971', { from: user1 })
+      await this.contract.createStar('Star power 103!', 'I love my wonderful star', 'ra_032.155', 'dec_121.874', 'mag_245.978', { from: user1 })
+
       const starOwner = await this.contract.ownerOf(1, { from: user1 })
 
       assert.equal(starOwner, user1)
     })
+
+    it('user2 is the owner of the star after they buy it', async function() {
+      await this.contract.createStar('Star power 103!', 'I love my wonderful star', 'ra_032.155', 'dec_121.874', 'mag_245.978', { from: user1 })
+
+      await this.contract.putStarUpForSale(starId, starPrice, { from: user1 })
+
+      await this.contract.buyStar(starId, { from: user2, value: starPrice, gasPrice: 0 })
+      assert.equal(await this.contract.ownerOf(starId), user2)
+    })
+
+    it('user2 ether balance changed correctly', async function() {
+      await this.contract.createStar('Star power 103!', 'I love my wonderful star', 'ra_032.155', 'dec_121.874', 'mag_245.978', { from: user1 })
+
+      await this.contract.putStarUpForSale(starId, starPrice, { from: user1 })
+
+      let overpaidAmount = web3.toWei(.05, 'ether')
+      const balanceBeforeTransaction = web3.eth.getBalance(user2)
+      await this.contract.buyStar(starId, { from: user2, value: overpaidAmount, gasPrice: 0 })
+      const balanceAfterTransaction = web3.eth.getBalance(user2)
+
+      assert.equal(balanceBeforeTransaction.sub(balanceAfterTransaction), starPrice)
+    })
+
   })
 })
