4b18475362ff1c91b265bec4c1c16453907997f6
Samuel Terra <samuelterra22@hotmail.com>
2018-10-23 22:40:02 -0300

    Implement new methods

--- project_5/smart_contracts/contracts/StarNotary.sol
+++ project_5/smart_contracts/contracts/StarNotary.sol
@@ -27,11 +27,24 @@ contract StarNotary is ERC721 {
 
     mapping(uint256 => Star) public tokenIdToStarInfo;
     mapping(uint256 => uint256) public starsForSale;
+    mapping(bytes32 => bool) public starHashMap;
 
+    // create a star
     function createStar(string _name, string _story, string ra, string dec, string mag) public {
 
+        // block (start) height as a token identify
         numTokens++;
         uint256 _tokenId = numTokens;
+
+        //check if _tokenId already exists
+        require(keccak256(abi.encodePacked(tokenIdToStarInfo[_tokenId].coordinates.dec)) == keccak256(""));
+
+        // checks if the coordinates are not empty
+        require(keccak256(abi.encodePacked(ra)) != keccak256(""));
+        require(keccak256(abi.encodePacked(dec)) != keccak256(""));
+        require(keccak256(abi.encodePacked(mag)) != keccak256(""));
+        require(_tokenId != 0);
+        require(!starExists(ra, dec, mag));
 
         // add coordinates in struct
         Coordinates memory coordinates = Coordinates(ra, dec, mag);
@@ -39,9 +52,13 @@ contract StarNotary is ERC721 {
 
         tokenIdToStarInfo[_tokenId] = newStar;
 
+        // set star hash as true
+        starHashMap[keccak256(abi.encodePacked(ra, dec, mag))] = true;
+
         _mint(msg.sender, _tokenId);
     }
 
+    // set star for sale
     function putStarUpForSale(uint256 _tokenId, uint256 _price) public {
         require(this.ownerOf(_tokenId) == msg.sender);
 
@@ -63,5 +80,25 @@ contract StarNotary is ERC721 {
         if (msg.value > starCost) {
             msg.sender.transfer(msg.value - starCost);
         }
+    }
+
+    // verify if already exists
+    function starExists(string ra, string dec, string mag) public view returns (bool) {
+        return starHashMap[keccak256(abi.encodePacked(ra, dec, mag))];
+    }
+
+    // return star info
+    function tokenIdToStarInfo(uint256 _tokenId) public view returns (string, string, string, string, string) {
+        return (
+        tokenIdToStarInfo[_tokenId].name,
+        tokenIdToStarInfo[_tokenId].story,
+        tokenIdToStarInfo[_tokenId].coordinates.ra,
+        tokenIdToStarInfo[_tokenId].coordinates.dec,
+        tokenIdToStarInfo[_tokenId].coordinates.mag);
+    }
+
+    // simple override mint method from parent
+    function mint(uint256 _tokenId) public {
+        super._mint(msg.sender, _tokenId);
     }
 }
